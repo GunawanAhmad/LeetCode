6553d0ff64c8eb499e0b89929b75099e67127ce5
Gunawan Nur Ahmad <Gunawanfsdev@gmail.com>
2021-12-25 13:06:38 +0700

    add remove duplicate from sorted array problem

---
+++ removeDuplicateFromSortedArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let compared = null;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == compared) {
+      nums.splice(i, 1);
+      i--;
+    }
+    compared = nums[i];
+  }
+  console.log(nums);
+};
+
+let arr = [1, 1, 2];
+removeDuplicates(arr);
